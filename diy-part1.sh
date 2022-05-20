d8ecc00f8d42da28d66c226b6b54494cd3388b8a
stonesun <wavesun@qq.com>
2022-05-20 11:27:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -35,4 +35,4 @@
 # git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
